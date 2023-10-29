69668af50f3dc3f230a6dc385250424cb6b39e63
Mohammad Daoud <m.a.daoud99@gmail.com>
2023-10-29 22:42:47 +0300

    refactor

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+let amount;
+let csrfToken;
+console.log("sending request")
+$.ajax({
+    type: "GET",
+    url: "CliQIntegration.php",
+    success: function (response) {
+        // Close the loading popup
+        const jsonResponse = JSON.parse(response);
+        console.log(jsonResponse);
+        console.log(jsonResponse.amount);
+
+        // Assign the value to the amount variable
+        amount = jsonResponse.amount;
+        csrfToken = jsonResponse.token;
+        $('#amount').attr('value', amount);
+        $('#amountText').html(amount + " دينار أردني ")
+        $('#token').attr('value', csrfToken);
+        // Now, you can use the "amount" variable elsewhere in your code
+        console.log(amount);
+    },
+    error: function (xhr, status, error) {
+        Swal.fire({
+            icon: 'error',
+            title: '',
+            html: '<p>حدث خطأ</p>',
+            confirmButtonColor: '#c72234',
+            confirmButtonText: 'حسناً'
+        });
+        // Handle errors
+        console.error(error);
+    }
+});
+
 document.getElementById('RAliasType').addEventListener('change', function () {
-    var RAliasType = this.value;
-    var RAliasValueInput = document.getElementById('RAliasValue');
+    let RAliasType = this.value;
+    let RAliasValueInput = document.getElementById('RAliasValue');
 
     if (RAliasType === 'MOBL') {
         RAliasValueInput.type = 'tel';
@@ -17,46 +51,40 @@ $(document).ready(function () {
         // Show a loading popup without the "Okay" button
         Swal.fire({
             title: '',
-            showConfirmButton: false, // Hide the "Okay" button
+            showConfirmButton: false,
             html: '<p>جار إرسال الطلب</p> <div class="loading-dots">' +
                 '<p style="font-size: 18px">الرجاء الذهاب الى تطبيق البنك الخاص بك لقبول طلب</p>' +
                 '<p style="color: red; font-size: 16px; margin-bottom: 5px">الرجاء عدم اغلاق التطبيق</p>' +
                 '<div></div>\n' +
                 '<div></div>\n' +
                 '<div></div>\n' +
-                '<p style="font-size: 16px; margin-top: 10px; color: red" id="countdown"></p>' +
+                '<p style="font-size: 16px; margin-top: 10px; color: red" id="countdown">الوقت المتبقي: <span></span></p>' +
                 '</div>',
-            allowOutsideClick: false
+            allowOutsideClick: false,
+            showLoaderOnConfirm: false, // Disable the loader
+            timer: 300000,
+            timerProgressBar: false,
+            didOpen: () => {
+                const span = Swal.getHtmlContainer().querySelector('span');
+                timerInterval = setInterval(() => {
+                    const timeLeft = Swal.getTimerLeft();
+                    const minutes = Math.floor(timeLeft / 60000);
+                    const seconds = Math.floor((timeLeft % 60000) / 1000);
+                    span.textContent = `${minutes} دقيقة و ${seconds} ثانية`;
+                }, 1000);
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            }
+        }).then((result) => {
+            if (result.dismiss === Swal.DismissReason.timer) {
+                console.log('I was closed by the timer');
+            }
         });
 
+
 // Set the countdown timer for 5 minutes (300,000 milliseconds)
-        let countdownTime = 5 * 60 * 1000; // 5 minutes in milliseconds
-        const countdownElement = document.getElementById("countdown");
 
-// Function to update the countdown and close the dialog when it reaches zero
-        function updateCountdown() {
-            const minutes = Math.floor(countdownTime / 60000);
-            const seconds = Math.floor((countdownTime % 60000) / 1000);
-
-            // Display the remaining time
-            countdownElement.textContent = `الوقت المتبقي لديك:  ${minutes}:${seconds}`;
-
-            if (countdownTime <= 0) {
-                // Close the Swal dialog when the countdown reaches zero
-                Swal.close();
-            } else {
-                countdownTime -= 1000; // Update the countdown every second
-            }
-        }
-
-// Update the countdown every second
-        const countdownInterval = setInterval(updateCountdown, 1000);
-
-// Clear the interval and close the dialog when the countdown is done
-        setTimeout(() => {
-            clearInterval(countdownInterval);
-            Swal.close();
-        }, countdownTime);
 
         // Get form data
         const formData = {
@@ -79,13 +107,15 @@ $(document).ready(function () {
 
                     if (jsonResponse.errorCode === "0") {
                         Swal.fire({
+                            icon: 'success',
                             title: '',
-                            html: '<p> تمت العملية بنجاح! ',
+                            html: ' تمت العملية بنجاح! ',
                             confirmButtonColor: '#71BF44',
                             confirmButtonText: 'حسناً'
                         });
                     } else {
                         Swal.fire({
+                            icon: 'error',
                             title: '',
                             html: '<p style="color: red">' + jsonResponse.description + '</p>',
                             confirmButtonColor: '#c72234',
@@ -93,11 +123,14 @@ $(document).ready(function () {
                         });
                     }
                 },
-                error: function (xhr, status, error) {
+                error: function (xhr, status, error, response) {
+                    const jsonResponse = JSON.parse(response);
+
                     Swal.close();
                     Swal.fire({
+                        icon: 'error',
                         title: '',
-                        html: '<p> حدث خطأ</p>',
+                        html: '<p> ' + jsonResponse.description + '</p>',
                         confirmButtonColor: '#c72234',
                         confirmButtonText: 'حسناً'
                     });
@@ -109,6 +142,7 @@ $(document).ready(function () {
         } catch (error) {
             Swal.close();
             Swal.fire({
+                icon: 'error',
                 title: '',
                 html: '<p> حدث خطأ</p>',
                 confirmButtonColor: '#c72234',
